0a01093e92c1b06fdf9d52b52cf7e4753fad7c38
vasyas <vasyl@stashuk.com>
2018-03-09 11:28:34 +0200

    remove incorrect typedfs

--- src/multipart.ts
+++ src/multipart.ts
@@ -1,17 +1,4 @@
-/** Based on @types/koa-multer */
-export interface File {
-    fieldname: string;
-    originalname: string;
-    encoding: string;
-    mimetype: string;
-    size: number;
-    destination: string;
-    filename: string;
-    path: string;
-    buffer: Buffer;
-}
-
 export class Multipart {
-    constructor(public readonly files: { [key: string]: File } = {}, public readonly fields: { [key: string]: string } = {}) {
+    constructor(public readonly files: { [key: string]: any } = {}, public readonly fields: { [key: string]: string } = {}) {
     }
 }
