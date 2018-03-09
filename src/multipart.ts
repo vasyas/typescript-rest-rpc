927970d80542087fb386b56d2d69beea737a8441
vasyas <vasyl@stashuk.com>
2018-03-09 11:03:01 +0200

    missing def for multipart

--- src/multipart.ts
+++ src/multipart.ts
@@ -1,3 +1,16 @@
+/** Based on @types/koa-multer */
+export interface File {
+    fieldname: string;
+    originalname: string;
+    encoding: string;
+    mimetype: string;
+    size: number;
+    destination: string;
+    filename: string;
+    path: string;
+    buffer: Buffer;
+}
+
 export class Multipart {
     constructor(public readonly files: { [key: string]: File } = {}, public readonly fields: { [key: string]: string } = {}) {
     }
