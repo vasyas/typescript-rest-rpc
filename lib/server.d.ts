de7eb76b375e28c25b6a2127f744efc769d5e975
vasyas <vasyl@stashuk.com>
2018-05-02 00:45:42 +0300

    custom context builder

--- lib/server.d.ts
+++ lib/server.d.ts
@@ -1,3 +1,6 @@
+/// <reference types="koa" />
 /// <reference types="koa-router" />
 import * as Router from "koa-router";
-export declare function createServerRouter(prefix: string, impl: object): Router;
+import { Context } from "koa";
+export declare type CreateContext = (ctx: Context) => any;
+export declare function createServerRouter(prefix: string, impl: object, createContext?: CreateContext): Router;
